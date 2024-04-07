be72622f36bd4820dfcc76493481b264a873b8f3
Ventsislav Peychev <del.busque@gmail.com>
2024-04-07 16:07:14 +0300

    Adding and updating records

--- ServiceNow-developer-TechWithPri/glideRecordMethods.js
+++ ServiceNow-developer-TechWithPri/glideRecordMethods.js
@@ -10,3 +10,32 @@ incRecords.query();
 while (incRecords.next()) {
     gs.info(incRecords.number + ' - ' + incRecords.short_description);
 }
+
+// Adding new record
+
+incRecords.initialize();
+incRecords.caller_id = "Abel Tuter";
+incRecords.short_description = 'Testing purpose 2';
+var test = incRecords.insert();
+gs.info(test);
+
+// Updating a record with if logic
+
+incRecords.addQuery('short_description', 'CONTAINS', 'hey test');
+incRecords.query();
+
+// while(incRecords.next()){
+// 	incRecords.short_description = 'Hey test me';
+// 	incRecords.update();
+// }
+
+if (incRecords.hasNext()) {
+    incRecords.short_description = 'Hey test me';
+    incRecords.updateMultiple();
+}
+
+// Updating a record or records with setValue method
+incRecords.addQuery('short_description', 'CONTAINS', 'test');
+incRecords.setValue('short_description', 'ServiceNow Tester');
+incRecords.updateMultiple();
+
