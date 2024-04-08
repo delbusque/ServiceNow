f00a29855b230555489222ed2c79d62ff5c7ba21
Ventsislav Peychev <del.busque@gmail.com>
2024-04-08 10:54:26 +0300

    Add some GlideSystem methods

---
+++ ServiceNow-developer-TechWithPri/GlideSystem.js
@@ -0,0 +1,8 @@
+gs.info('Testing info');
+gs.log('My log', 'Scripting');
+gs.error('An error occured');
+
+gs.print('Hello world');
+
+gs.print(gs.getUser().getUserRoles());
+gs.print(gs.getProperty('auxdb.db.name'));
