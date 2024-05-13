612d327859c1ca9151734ea4b85695da8d5991ca
delbusque <del.busque@gmail.com>
2024-05-13 12:09:49 +0300

    Call from Client Script Include

---
+++ ServiceNow_Developer-TechWithPri/clientScript-ScriptInclude.js
@@ -0,0 +1,34 @@
+// Script Include for fetching Incident details
+
+var incidentDetails = Class.create();
+incidentDetails.prototype = Object.extendsObject(AbstractAjaxProcessor, {
+
+    getAssignments: function () {
+
+        var gr = new GlideRecord('incident');
+        gr.addQuery('assigned_to', this.getParameter('sys_parm_assigned_to'));
+        gr.query();
+
+        return gr.getRowCount();
+    },
+
+    type: 'incidentDetails'
+});
+
+// Client Script for getting and displaying needed data
+
+function onChange(control, oldValue, newValue, isLoading, isTemplate) {
+    if (isLoading || newValue === '') {
+        return;
+    }
+
+    var ga = new GlideAjax('incidentDetails');
+    ga.addParam('sysparm_name', 'getAssignments');
+    ga.addParam('sys_parm_assigned_to', g_form.getValue('assigned_to'));
+    ga.getXMLAnswer(fetchCount);
+
+    function fetchCount(response) {
+        g_form.setValue('u_assignements', parseInt(response));
+    }
+
+}
